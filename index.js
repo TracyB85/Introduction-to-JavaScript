eadccdcd76c409e22b5ec7cf9356797c842551df
TracyB85 <tracy.braswelljr.tbj@gmail.com>
2021-06-08 23:46:43 -0400

    finish project

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-    /*add your code here*/
+function multiply(a, b){
+    return a * b
   }
 
 
@@ -74,8 +74,8 @@ Do the following:
 */
 
 function dogYears(humanYear){
-    dogYears = humanYear * 7
-    return dogYears
+    return  humanYear * 7
+    
 }
 
 
@@ -151,12 +151,36 @@ Use the game function below to do the following:
   
   HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
+var computer = Math.random();
+if (computer <= .33){
+  computer = "rock"
+} else if (computer > .33 && computer <= .66){
+  computer = "paper"
+} else {
+  computer = "scissors"
+}
 
 function game(user, computer){
-    /*add your code here*/
+  if (user === "rock" && computer === "rock"){
+    return "it's a tie"
+  }else if (user === "paper" && computer === "rock"){
+    return "you win!"
+  }else if (user === "scissors" && computer === "rock"){
+    return "you lose!"
+  }else if (user === "rock" && computer === "paper"){
+    return "you lose!"
+  }else if (user === "paper" && computer === "paper"){
+    return "it's a tie"
+  }else if (user === "scissors" && computer === "paper"){
+    return "you win!"
+  }else if (user === "rock" && computer === "scissors"){
+      return "you win!"
+  }else if (user === "paper" && computer === "scissors"){
+      return "you lose!"
+  }else if (user === "scissors" && computer === "scissors"){
+      return "it's a tie"
+  }
 }
-  
-  
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -169,8 +193,8 @@ Using the miles function below do the following:
   3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-    /*add your code here*/
+function miles(kilometers){
+  return kilometers * 0.621371
   }
 
 
@@ -183,8 +207,8 @@ Using the feet function below do the following:
   3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-    /*add your code here*/
+function feet(cm){
+    return cm / 30.48
   }
  
 
@@ -199,9 +223,10 @@ Using the annoyingSong function below do the following:
       "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-        /*add your code here*/
-  }
+function annoyingSong(bottles){
+  return `${bottles} bottles of soda on the wall, ${bottles} bottles of soda, take one down pass it around ${bottles - 1} bottles of soda on the wall`
+      
+}
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -219,9 +244,24 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
-  /*Your Code here */
+function grade(score){
+  if(score >= 90){
+    return 'you got an A'
   }
+  if(score >= 80){
+    return 'you got a B'
+  }
+  if(score >= 70){
+    return 'you got a C'
+  }
+  if(score >= 60){
+    return 'you got a D'
+  }
+  else return 'you got an F'
+  }
+
+
+  
   
   
 
